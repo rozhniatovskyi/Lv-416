cdf933b3f1b37dde58e03f64b23bd0f789acec3c
Rozhniatovskyi Rostyslav <r.rozhnyatovskyy@gmail.com>
2019-06-06 22:39:13 +0300

    add a task1

---
+++ task_1_rozhniatovkyi.py
@@ -0,0 +1,37 @@
+# Task 1
+name = input('Enter your name:')
+print('your name is  ', name)
+
+age = input('How old are you?')
+print('your age : ', age)
+
+where_do_you_live = input('Where do you live?')
+print('i live in:', where_do_you_live)
+# Task 2
+a = input('Enter a: ')
+b = input('Enter b: ')
+
+sum_of_two_variables = int(a) + int(b)
+print('Sum: ', sum_of_two_variables)
+
+subtraction = int(a) - int(b)
+print('Min: ', subtraction)
+
+# Task 3
+
+value_int = 10
+value_float = 8.4
+value_str = 'NO'
+
+big_int = value_int * 3.5
+print('big_int = ', big_int)
+
+value_float = value_float - 1
+print('value_float = ', value_float)
+
+print('value_int / value_float = ', value_int / value_float)
+print('big_int / value_float = ', big_int / value_float)
+
+value_str = ('NO' * 2) + ('Yes' * 3)
+
+print(value_str)
